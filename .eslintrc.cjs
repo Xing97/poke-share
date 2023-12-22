20fce21dd69b1f4e7745362b4998cdb0a9d6452e
Xing97 <xinglong997@gmail.com>
2023-12-22 20:20:54 +0100

    Initialize vite + react + typescript + tailwind

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'standard-with-typescript'
+  ],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'tailwind.config.js',
+    'vite.config.ts',
+    'postcss.config.js',
+    'vite-env.d.ts'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/explicit-function-return-type": "off"
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export default function App () {
+  return (
+    <h1 className='bg-red-500'>Pokezi</h1>
+  )
+}
