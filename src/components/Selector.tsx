d18c8272973bb4522b45783200398f05d3bbc977
Xing97 <xinglong997@gmail.com>
2024-02-04 22:45:53 +0100

    Fix duplicated id in Selector

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -5,7 +5,7 @@ interface Props<T extends string> {
   options: T[]
   selected: T
   setSelected: (option: T) => void
-  translation: string | Record<T, string>
+  translation: string
   enabled?: T[]
   className?: string
 }
@@ -23,16 +23,15 @@ export default function Selector<T extends string> (
           className='cursor-pointer rounded-full border border-slate-900 bg-slate-400 px-2 font-semibold tracking-wide hover:bg-slate-500 has-[:disabled]:cursor-default has-[:disabled]:border-slate-600 has-[:checked]:bg-slate-900 has-[:disabled]:bg-inherit has-[:checked]:text-white has-[:disabled]:text-slate-500 dark:border-slate-100 dark:bg-slate-500 dark:hover:bg-slate-400 dark:has-[:checked]:bg-slate-100 dark:has-[:checked]:text-black'
         >
           <input
-            id={option}
+            name={id}
             className='hidden'
             type='radio'
-            name={id}
             value={option}
             onChange={() => { setSelected(option) }}
             checked={option === selected}
             disabled={enabled !== undefined && !enabled.includes(option)}
           />
-          { typeof translation === 'string' ? t(translation + '.' + option) : translation[option]}
+          {t(translation + '.' + option)}
         </label>
       ))}
     </div>
