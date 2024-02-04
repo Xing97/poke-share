b47ea97a235635e9d470632209af6469ffba880c
Xing97 <xinglong997@gmail.com>
2024-02-04 20:25:34 +0100

    Implement PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,93 @@
-import million from 'million/compiler';
 import react from '@vitejs/plugin-react-swc';
+import million from 'million/compiler';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from "vite-plugin-pwa";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  plugins: [
+    million.vite({ auto: true }),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.svg',
+        'apple-touch-icon.png',
+        'physical.png',
+        'special.png',
+        'status.png'
+      ],
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/pokeapi\.co\/api\/v2\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'pokeapi-cache',
+              expiration: {
+                maxEntries: 10000,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/raw\.githubusercontent\.com\/PokeApi\/sprites\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'pokeapi-sprites-cache',
+              expiration: {
+                maxEntries: 10000,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
+      manifest: {
+        name: "Pokezi",
+        short_name: "Pokezi",
+        description: "Web to share Pokemon sets with anyone",
+        theme_color: "#334155",
+        background_color: "#0f172a",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ],
+      }
+    })
+  ],
   build: {
     target: 'ESNext'
   },
