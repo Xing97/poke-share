804c21fa5cbb29688b71789b51b8d4672fd421aa
Xing97 <xinglong997@gmail.com>
2024-02-05 19:51:33 +0100

    Fix manifest?

--- vite.config.ts
+++ vite.config.ts
@@ -61,25 +61,25 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any"
           },
           {
-            src: "/pwa-512x512.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any"
           },
           {
-            src: "/pwa-maskable-192x192.png",
+            src: "pwa-maskable-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable"
           },
           {
-            src: "/pwa-maskable-512x512.png",
+            src: "pwa-maskable-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable"
