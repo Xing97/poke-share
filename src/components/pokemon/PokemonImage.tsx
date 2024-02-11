f968e019af606d946ce8a759c19cc5804dce23d3
Xing97 <xinglong997@gmail.com>
2024-02-11 13:29:16 +0100

    Display a placeholder in case of image error

--- src/components/pokemon/PokemonImage.tsx
+++ src/components/pokemon/PokemonImage.tsx
@@ -1,7 +1,10 @@
 import { useI18nName } from '@/hooks/useI18nName'
 import FemaleIcon from '@/icons/FemaleIcon'
 import MaleIcon from '@/icons/MaleIcon'
+import PokeBallIcon from '@/icons/PokeBallIcon'
+import QuestionIcon from '@/icons/QuestionIcon'
 import { Gender, type Pokemon } from '@/model/pokemon'
+import { useState } from 'react'
 
 interface Props {
   pokemon: Pokemon
@@ -9,19 +12,32 @@ interface Props {
 
 export default function PokemonImage ({ pokemon }: Props): JSX.Element {
   const i18n = useI18nName()
+  const [pokemonImageError, setPokemonImageError] = useState(false)
+  const [itemImageError, setItemImageError] = useState(false)
 
   return (
-    <div className='relative'>
-      <img className='img-pokemon' src={pokemon.image} alt={i18n(pokemon.name)} />
-      {pokemon.gender === Gender.Male && <MaleIcon className='absolute left-0 top-0' height='32px' width='32px' />}
-      {pokemon.gender === Gender.Female && <FemaleIcon className='absolute left-0 top-0' height='32px' width='32px' />}
-      {pokemon.item != null &&
-        <img
-          className='img-item absolute bottom-0 right-0'
-          src={pokemon.item.image}
-          alt={i18n(pokemon.item.name)}
-          title={i18n(pokemon.item.name)}
+    <div className='relative flex h-52 w-52 items-center justify-center'>
+      {pokemonImageError
+        ? <PokeBallIcon className='h-20 w-20' />
+        : <img
+            className='img-pokemon h-full w-full' src={pokemon.image} alt={i18n(pokemon.name)}
+            onError={() => { setPokemonImageError(true) }}
         />}
+      {pokemon.gender === Gender.Male && <MaleIcon className='absolute left-0 top-0 h-8 w-8' />}
+      {pokemon.gender === Gender.Female && <FemaleIcon className='absolute left-0 top-0 h-8 w-8' />}
+      {pokemon.item != null &&
+        (itemImageError
+          ? <QuestionIcon
+              className='absolute bottom-0 right-0 h-1/6 w-1/6'
+              title={i18n(pokemon.item.name)}
+          />
+          : <img
+              className='img-item absolute bottom-0 right-0 h-1/4 w-1/4'
+              src={pokemon.item.image}
+              alt={i18n(pokemon.item.name)}
+              title={i18n(pokemon.item.name)}
+              onError={() => { setItemImageError(true) }}
+          />)}
     </div>
   )
 }
