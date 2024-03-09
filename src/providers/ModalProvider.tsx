8aaf4f6443e09da1618905ddf700b4fe6e3c3957
Xing97 <xinglong997@gmail.com>
2024-03-09 16:50:23 +0100

    Change modal style

--- src/providers/ModalProvider.tsx
+++ src/providers/ModalProvider.tsx
@@ -19,9 +19,9 @@ export default function ModalProvider (): JSX.Element | null {
     <dialog
       ref={dialog}
       onClick={closeModal}
-      className='scrollbar-thin inset-0 m-auto max-w-4xl animate-zoom-in rounded-lg border border-slate-600 bg-slate-200 text-black animate-duration-fast backdrop:animate-fade-in backdrop:bg-black/55 backdrop:animate-duration-fast dark:bg-slate-800 dark:text-white'
+      className='scrollbar-thin inset-0 max-w-4xl animate-zoom-in border border-slate-600 bg-slate-200 text-black animate-duration-fast backdrop:animate-fade-in backdrop:bg-black/55 backdrop:animate-duration-fast dark:bg-slate-800 dark:text-white md:rounded-lg'
     >
-      <div className='size-full h-fit py-6 pl-6 pr-10' onClick={e => { e.stopPropagation() }}>
+      <div className='size-full h-fit p-6' onClick={e => { e.stopPropagation() }}>
         <button
           className='absolute right-2 top-2 rounded p-0.5 hover:bg-black/25'
           onClick={closeModal}
